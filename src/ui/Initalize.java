018301d562e70f8ffc87b83fcf9db45d77117fc3
Matthew Garneau <sadzune@gmail.com>
2018-09-17 12:33:01 -0700

    Restarting project

--- src/ui/Initalize.java
+++ src/ui/Initalize.java
@@ -1,22 +1,11 @@
 package ui;
 
-import ui.windows.ApplicationWindow;
 
-import javax.swing.*;
 
 public class Initalize{
 
-    final static int WIDTH = 1000;
-    final static int HEIGHT = 700;
-
 
     public static void main(String[] args){
-        ApplicationWindow appWin = new ApplicationWindow(WIDTH, HEIGHT);
-
-        //debugging
-        JInternalFrame dummyFrame = new JInternalFrame();
-        appWin.addInternalFrame(dummyFrame);
-
     }
     /*
     static JTextArea mainTextArea;
